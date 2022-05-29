f9c9c0a2a5fbee1b1883df9b3432f352c457b54b
chathuranga <chathuranga@freshworks.io>
2022-05-29 13:49:54 +0530

    added more columns

--- src/packages/ah_NFT_sale/ahNFTSale.model.js
+++ src/packages/ah_NFT_sale/ahNFTSale.model.js
@@ -14,6 +14,9 @@ const AhNFTSale = sequelize.define('ah_nft_sale', {
   tnx_sol_amount: { type: DataTypes.FLOAT, allowNull: true, defaultValue: 0.0 },
   tnx_usd_amount:  { type: DataTypes.FLOAT, allowNull: true, defaultValue: 0.0 },
   offer_id: { type: DataTypes.STRING, allowNull: true },
+  metadata: { type: DataTypes.JSON, allowNull: true},
+  nft_name:  { type: DataTypes.STRING, allowNull: true, defaultValue: null },
+  url:  { type: DataTypes.STRING, allowNull: true, defaultValue: null }
 }, {
   paranoid: true,
   timestamps: true
